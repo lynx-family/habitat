86e0f2e31dbeefcc5f35a8e78fd5aa5f944e4bc2
coolkiid <73600915+coolkiid@users.noreply.github.com>
2026-03-18 15:47:41 +0800

    refactor: make default exception more structural for expansion

--- core/exceptions.py
+++ core/exceptions.py
@@ -2,6 +2,36 @@
 # Licensed under the Apache License Version 2.0 that can be found in the
 # LICENSE file in the root directory of this source tree.
 
+import json
+from typing import Optional
+
 
 class HabitatException(Exception):
     """Base exception in habitat"""
+
+    def __init__(
+        self,
+        message: str,
+        *,
+        cause: Optional[Exception] = None,
+        hint: Optional[str] = None,
+        context: Optional[dict] = None,
+    ):
+        self.message = message
+        self.cause = cause
+        self.hint = hint
+        self.context = context
+
+    def __str__(self) -> str:
+        e = {"message": self.message}
+
+        if self.cause:
+            e["cause"] = str(self.cause)
+
+        if self.hint:
+            e["hint"] = self.hint
+
+        if self.context:
+            e["context"] = self.context
+
+        return json.dumps(e, indent=2, ensure_ascii=False)
